f03ddc739225aa8e06740cff17ed914e15932cd4
neberhardt123 <42151168+neberhardt123@users.noreply.github.com>
2020-11-19 13:58:29 -0800

    fixed spam

--- compareUserBaselines.ps1
+++ compareUserBaselines.ps1
@@ -3,14 +3,14 @@ import-module activedirectory
  
 $Logs = ".\logs.txt"
 $quit = $false
-
+$temp = @()
 while($quit -eq $false) {
 Write-Host "1. Create Baseline users to a txt in current dir" -ForegroundColor Green;
 Write-Host "2. Compare current users to baseline users" -ForegroundColor Green;
 Write-Host "3. Exit" -ForegroundColor Green;
 
 
-$choice = read-host "Choose wisely"   
+$choice = read-host "Choose something"   
     if ($choice -eq 1)
         {
 
@@ -27,22 +27,29 @@ $choice = read-host "Choose wisely"
                 $global:PathCompare = ".\compareUsers.txt"
 				Get-ADUser -Filter * | select sAMAccountName | Out-File -FilePath $PathCompare
         
-
+                
 
  
-                #Compare-Object $(Get-Content $Path) $(Get-Content $PathCompare) | Add-Content -Path $Logs
-                    
-
-                    if (Compare-Object $(Get-Content $Path) $(Get-Content $PathCompare))
+                $spam = @(Compare-Object $(Get-Content $Path) $(Get-Content $PathCompare) | Select-Object -ExpandProperty InputObject)
+                    if ($spam)
                         {
-                        $date = Get-Date
-                        Write-Host $date -NoNewline 
-                        Write-Host " New User Detected"
-                        Add-Content -Path $Logs -Value "Possible new user: " -NoNewLine
-                        Compare-Object $(Get-Content $Path) $(Get-Content $PathCompare) | Add-Content -Path $Logs
-                        
+                        if(Compare-Object $spam $temp) {
+                            
+                            $date = Get-Date
+                            Write-Host $date -NoNewline 
+                            Write-Host " New User(s) Detected: " -NoNewline
+                            Get-Date | Add-Content -Path $Logs -NoNewline
+                            Add-Content -Path $Logs -Value ": Possible new user: " -NoNewLine
+                            
+                            #Compare-Object $(Get-Content $Path) $(Get-Content $PathCompare) | Select-Object -ExpandProperty InputObject | Add-Content -Path $Logs
+                            $temp = @(Compare-Object $(Get-Content $Path) $(Get-Content $PathCompare) | Select-Object -ExpandProperty InputObject)
+                            Add-Content -Value $temp -Path $Logs
+                            Write-Host $temp
+                            
                         }
+                    }
                   Start-Sleep -seconds 25
+                    
 
                 }  
         }
